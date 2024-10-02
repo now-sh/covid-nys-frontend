7e32aefbdf898b90391b6e6b70caf9de97893b91
casjay <git-admin@casjaysdev.pro>
2024-10-02 12:11:31 -0400

    📃 Deployed: src/main.js 📃
    Deployed: src/main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-import "bootswatch/dist/darkly/bootstrap.min.css";
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-import VueRouter from 'vue-router';
-import BootstrapVue from 'bootstrap-vue';
-import IconsPlugin from 'bootstrap-vue';
 import "bootstrap";
 import "../node_modules/bootswatch/dist/darkly/bootstrap.min.css";
 import "jquery";
