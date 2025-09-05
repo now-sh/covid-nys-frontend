4f67000b6437aee1607dc5161920cc58bdb91142
casjay <git-admin@casjaysdev.pro>
2025-09-05 15:01:27 -0400

    📃 Deployed: src/main.js 📃
    Deployed: src/main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootswatch/dist/darkly/bootstrap.min.css'
+import './assets/dracula-theme.css'
 import * as bootstrap from 'bootstrap'
 
 window.bootstrap = bootstrap
